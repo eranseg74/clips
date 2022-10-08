8e5fbfabcb4ca360fd82349a9ba655d6a4e5f380
Eran Segal <eranseg74@gmail.com>
2022-10-08 21:22:27 +0300

    Delete Dummy

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -8,7 +8,5 @@ import { Component, OnInit } from '@angular/core';
 export class UploadComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {
-    console.log('Eran');
-  }
+  ngOnInit(): void {}
 }
